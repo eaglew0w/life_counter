8bf51b1200b815d50c99d6983ef8c953472a0728
eaglew0w <93960702+eaglew0w@users.noreply.github.com>
2024-07-24 10:12:15 +0900

    画面ロックしないよう修正 #20

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 import '../screens/life_counter_app.dart';
 
 void main() {
   runApp(const ProviderScope(child: LifeCounterApp()));
+  WakelockPlus.enable();
 }
