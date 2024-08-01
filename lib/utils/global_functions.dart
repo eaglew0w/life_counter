c28d665e56eee0c734992e5a56ccb2c0f32ebba0
eaglew0w <93960702+eaglew0w@users.noreply.github.com>
2024-08-02 01:35:03 +0900

    背景の状態を追加 #4

--- lib/utils/global_functions.dart
+++ lib/utils/global_functions.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:life_counter/constants/constants.dart';
+import 'package:life_counter/models/background_state.dart';
+
 String addAbsoluteValueText(int value) {
   String retString = '';
   if (value == 0) {
@@ -8,4 +12,25 @@ String addAbsoluteValueText(int value) {
     retString = '$value';
   }
   return retString;
+}
+
+Color getBackgroundColor(BackgroundState backgroundState) {
+  Color retColor = backgroundColorDefault;
+
+  switch (backgroundState) {
+    case BackgroundState.unset:
+      retColor = backgroundColorDefault;
+      break;
+    case BackgroundState.day:
+      retColor = backgroundColorDay;
+      break;
+    case BackgroundState.night:
+      retColor = backgorundColorNight;
+      break;
+    default:
+      retColor = backgroundColorDefault;
+      break;
+  }
+
+  return retColor;
 }
