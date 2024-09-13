cf394df9c3afb4516591293e46eaf2a3c241283c
eaglew0w <93960702+eaglew0w@users.noreply.github.com>
2024-09-14 02:18:35 +0900

    ボタン配置調整した画面採用 #35

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
-import 'package:life_counter/screens/life_counter_app.dart';
+import 'package:life_counter/screens/life_counter_app_2player.dart';
 
 void main() {
-  runApp(const ProviderScope(child: LifeCounterApp()));
+  runApp(const ProviderScope(child: LifeCounterApp2Player()));
   WakelockPlus.enable();
 }
