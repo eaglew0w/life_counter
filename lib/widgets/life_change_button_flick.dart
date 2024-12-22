840f3d5173f54fe69c0f90a03ee9a71990a92150
eaglew0w <93960702+eaglew0w@users.noreply.github.com>
2024-12-22 13:45:54 +0900

    フリック判定を厳密に判定するボタンを作成 #56 ついでにボタンをstatelessに変更  #55

---
+++ lib/widgets/life_change_button_flick.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LifeChangeButtonFlick extends StatelessWidget {
+  final String text;
+  final Alignment alignment;
+  final Color textColor;
+  final int flickThreshold;
+  final VoidCallback onFlickFunc;
+  final VoidCallback onTapFunc;
+
+  const LifeChangeButtonFlick({
+    required this.text,
+    required this.flickThreshold,
+    required this.onFlickFunc,
+    required this.onTapFunc,
+    this.alignment = Alignment.center,
+    this.textColor = Colors.white,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Offset? startPosition;
+    return GestureDetector(
+      onTap: () => {onTapFunc()},
+      onPanStart: (details) {
+        startPosition = details.localPosition;
+      },
+      onPanEnd: (details) {
+        if (startPosition != null) {
+          double distance = (details.localPosition - startPosition!).distance;
+          if (distance > flickThreshold) {
+            onFlickFunc();
+          }
+        }
+        startPosition = null;
+      },
+      child: ElevatedButton(
+        onPressed: null, // onPressedは使用せずGestureDetectorで操作
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+        ),
+        child: Align(
+          alignment: alignment,
+          child: Text(
+            text,
+            style: TextStyle(color: textColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
