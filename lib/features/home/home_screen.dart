449289e83a9e5ecc7f11972cdd6752fa55f4c3ce
eaglew0w <clonemanexe@gmail.com>
2025-01-03 11:54:53 +0900

    フォルダを移動

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:life_counter/features/lifecounter2player/life_counter_2_player.dart';
+import 'package:life_counter/features/life_counter/2player/life_counter_2_player.dart';
 import 'package:life_counter/shared/constants/constants.dart';
 
 class HomeScreen extends StatelessWidget {
