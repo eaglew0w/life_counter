fabf9f497b19631589de38c7c718e38744e98bd9
eaglew0w <93960702+eaglew0w@users.noreply.github.com>
2025-02-09 23:49:42 +0900

    初期化フラグを追加、それを見てボタンのアイコンを取得するよう修正 #91

--- lib/shared/widgets/button/theme_mode_change_button/theme_mode_change_button.dart
+++ lib/shared/widgets/button/theme_mode_change_button/theme_mode_change_button.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:life_counter/shared/notifiers/theme_mode_notifier.dart';
+import 'package:life_counter/shared/models/theme_mode_state.dart';
+import 'package:life_counter/shared/notifiers/theme_mode_state_notifier.dart';
 import 'package:life_counter/shared/utils/global_functions.dart';
 
 class ThemeModeChangeButton extends ConsumerWidget {
-  final NotifierProvider<ThemeModeNotifier, ThemeMode> themeModeProvider;
+  final NotifierProvider<ThemeModeStateNotifier, ThemeModeState>
+      themeModeStateProvider;
 
   const ThemeModeChangeButton({
-    required this.themeModeProvider,
+    required this.themeModeStateProvider,
     super.key,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ThemeMode themeMode = ref.watch(themeModeProvider);
-    final ThemeModeNotifier themeModeNotifier =
-        ref.read(themeModeProvider.notifier);
+    final ThemeModeState themeModeState = ref.watch(themeModeStateProvider);
+    final ThemeModeStateNotifier themeModeStateNotifier =
+        ref.read(themeModeStateProvider.notifier);
     return ElevatedButton(
       onPressed: () {
-        themeModeNotifier.setNextThemeMode();
+        themeModeStateNotifier.setNextThemeMode();
       },
       onLongPress: () {
-        themeModeNotifier.reset();
+        themeModeStateNotifier.reset();
       },
       style: ElevatedButton.styleFrom(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
       ),
-      child: getThemeModeIcon(themeMode),
+      child: getThemeModeIcon(themeModeState),
     );
   }
 }
