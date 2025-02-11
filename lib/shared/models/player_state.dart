1915108aa41b125f60f8e2a5df4547bb5358e00e
eaglew0w <93960702+eaglew0w@users.noreply.github.com>
2025-02-11 13:00:47 +0900

    Timerをnotifierに持たせてcopyWith関数を作成 #98

--- lib/shared/models/player_state.dart
+++ lib/shared/models/player_state.dart
@@ -3,7 +3,13 @@ import 'dart:async';
 class PlayerState {
   int life;
   int lifeChange;
-  Timer? timer;
 
-  PlayerState({required this.life, this.lifeChange = 0, this.timer});
+  PlayerState({required this.life, this.lifeChange = 0});
+
+  PlayerState copyWith(
+      {int? life, int? lifeChange, Timer? timer}) {
+    return PlayerState(
+        life: life ?? this.life,
+        lifeChange: lifeChange ?? this.lifeChange);
+  }
 }
